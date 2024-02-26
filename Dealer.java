b2a0d79f42094c5bd3b2811e7c7e555de593571e
EliyaYa123 <157639716+EliyaYa123@users.noreply.github.com>
2024-02-27 00:07:25 +0200

    Add files via upload

--- Dealer.java
+++ Dealer.java
@@ -292,5 +292,13 @@ public class Dealer implements Runnable {
                 winners.add(p.id);
             }
         }
+        if (winners.size()==1)
+            System.out.println("The winner is: ");
+        else
+            System.out.println("The winners are: ");
+            
+        for (Integer winner: winners){
+            System.out.println("Player " + winner);
+        }
     }
 }
