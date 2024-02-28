ca16d81d002603057a4f74ae4b900ee34564b562
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-02-28 17:29:56 +0200

    Update ThreadSafeLinkedList.java

--- ThreadSafeLinkedList.java
+++ ThreadSafeLinkedList.java
@@ -1,35 +1,74 @@
 package bguspl.set.ex;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
-public class ThreadSafeLinkedList {
-    protected LinkedList<LinkPlayerSet> list;
+import bguspl.set.Env;
 
-    public ThreadSafeLinkedList(){
-        this.list = new LinkedList<LinkPlayerSet>();
+public class ThreadSafeList {
+
+    protected ArrayList<Integer> list = new ArrayList<>();
+    private final Env env;
+    private int slot;
+
+    public ThreadSafeList(Env _env, int _slot){
+        this.env = _env;
+        this.slot = _slot;
     }
 
 
-    public synchronized boolean add(LinkPlayerSet newLink) {
-        return this.list.add(newLink);
+    public synchronized boolean add(int value) {
+        if (!list.contains(value)) { //EYTODO maybe change?
+            list.add(value);
+            env.ui.placeToken(value, this.slot);
+            return true;
+        }
+        return false;
     }
 
-    public synchronized LinkPlayerSet removeFirst() {
-        return this.list.removeFirst();
+    public synchronized boolean remove(int value) {
+        if (list == null) return false;
+        boolean exists = list.remove(Integer.valueOf(value));
+        if(exists){
+            env.ui.removeToken(value, this.slot);
+        }
+        return exists;
     }
 
-    public synchronized boolean remove(LinkPlayerSet removedLink) {
-        if (this.list.isEmpty() || !this.list.contains(removedLink)) return false;
-        return this.list.remove(removedLink);
-    }
+    public synchronized int[] removeAll() { //removes all players from this slot, removes their tokens from ui, and return their ids
+        if (list == null) return null;
 
-    public synchronized boolean removeAll() { //removes all players from this slot, removes their tokens from ui, and return their ids
-        if (list == null) return true;
+        int[] players = new int[this.list.size()];
+        for(int i=0;i<players.length;i++){
+            players[i] = this.list.get(i).intValue();
+            env.ui.removeToken(players[i], this.slot);
+        }
         this.list.clear();
-        return true;
+        return players;
     }
 
-    public synchronized boolean isEmpty() {
-        return (list.peekFirst()==null);
+    public synchronized boolean contains(int value) {
+        if (list == null) return false;
+        return list.contains(value);
+    }
+
+    public synchronized int size() {
+        if (list == null) return -1;
+        return list.size();
+    }
+
+    public synchronized int[] getPlayers() { //TODO
+        int[] players = new int[this.list.size()];
+        for(int i=0;i<players.length;i++){
+            players[i] = this.list.get(i).intValue();
+        }
+        return players;
+    }
+
+    //EYTODO delete later, used for testing
+    public void print() {
+        for(int i=0;i<this.list.size();i++){
+            System.out.println(this.list.get(i)); 
+        }
     }
 }
+
