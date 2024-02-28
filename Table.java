6624adfd57753883d5177a374cafd31b17fb562d
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-02-28 10:28:26 +0200

    Update Table.java

--- Table.java
+++ Table.java
@@ -4,7 +4,6 @@ import bguspl.set.Env;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
